cdabfc1fc4a6916e3afcb10f4512789b54ecce89
Oscar Lopez <oscarlopez@despegar.com>
2016-05-27 16:51:26 -0300

    se agrega propiedad jsdoc al config/env

--- routes/statics.js
+++ routes/statics.js
@@ -1,23 +1,28 @@
 "use strict";
 
 const Path = require("path");
+const config = require("config");
+const debug = require("debug")("api:static")
 /**
  * @class
  * @param {object} server Hapi Server
  */
 function Statics(server){
     //JS DOC 3. Se compila con gulp
-    server.route({
-        method: 'GET',
-        path: '/jsdocs/{param*}',
-        handler: {
-            directory: {
-                path: Path.resolve(__dirname,'../public/jsdocs'),
-                redirectToSlash: true,//Clave dejar el redirectToSlash
-                index: true
+
+    if(config && config.jsdoc && config.jsdoc.enable){
+        server.route({
+            method: 'GET',
+            path: '/jsdocs/{param*}',
+            handler: {
+                directory: {
+                    path: Path.resolve(__dirname,'../public/jsdocs'),
+                    redirectToSlash: true,//Clave dejar el redirectToSlash
+                    index: true
+                }
             }
-        }
-    });
+        });
+    }
 
 }
 
