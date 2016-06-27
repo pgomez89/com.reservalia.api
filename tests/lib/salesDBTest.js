160318e38c30f1f3cedaa2d5314d188d5f849f47
Oscar Lopez <oscarlopez@despegar.com>
2016-06-27 16:59:33 -0300

    salesDB test

--- tests/lib/salesDBTest.js
+++ tests/lib/salesDBTest.js
@@ -1,14 +1,29 @@
 "use strict";
 
-const assert = require("assert");
+const debug = require("debug")("api:tests:salesDBTest");
 
-const salesDB = require("../../lib/SalesDB");
+const salesDB = require("../../lib/salesDB.js");
 
-describe("Testeando SalesDB", () => {
-    it("Deberia traer todas las ventas por hotelID", () => {
-        salesDB.getSalesByHotelId({hotelId:"331131"},{},{}, (err,sales) => {
-            assert(sales.length > 0);
-            assert.equal(null,err);
-        });
+const chai = require('chai')
+    , expect = chai.expect
+    , should = chai.should();
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+/**
+ * Arrow Functions
+ * Passing arrow functions to Mocha is discouraged.
+ * Their lexical binding of the this value makes them unable to access the Mocha context,
+ * and statements like this.timeout(1000); will not work inside an arrow function.
+ */
+describe("salesDB", () => {
+
+    it("Deberia retornar todas las ventas teniendo en cuenta los filtros y ordenamiento", () => {
+        return salesDB.getSales({page:1,limit:10}, {}, {}).should.eventually.contain.all.keys(['docs', 'total','limit','page','pages']);
     });
+
+    it("Deberia retorna todas las ventas de un hotel en particular", () => {
+        return salesDB.getSalesByHotelId({hotelId:331131,page:1,limit:10},{},{}).should.eventually.have.all.keys(['docs', 'total','limit','page','pages']);
+    });
+
 });
