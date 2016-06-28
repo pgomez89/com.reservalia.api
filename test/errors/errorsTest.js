36516d4274390c20189c141a6815a973cac0498a
Oscar Lopez <oscarlopez@despegar.com>
2016-06-28 11:08:13 -0300

    se cambia la carpeta tests a test para estandar de mocha

--- test/errors/errorsTest.js
+++ test/errors/errorsTest.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const assert = require("assert");
-const Errors = require("../lib/errors");
+const Errors = require("../../lib/errors");
 const Boom = require("boom");
 
 describe("Controllers Errors Test", () => {
--- test/sorting/SalesSortingTest.js
+++ test/sorting/SalesSortingTest.js
@@ -2,7 +2,7 @@
 
 var assert = require('assert');
 
-var sort = require("../sorting/Sorting");
+var sort = require("../../sorting/Sorting");
 describe('SalesSortingTest', function() {
     it('Deberia retornar un objeto con los filtros solo con los valores -1 y 1 ', function () {
 
