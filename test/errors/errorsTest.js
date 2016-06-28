e6ffca1a01eea98d83d0758bd7d3c27152e579eb
Oscar Lopez <oscarlopez@despegar.com>
2016-06-28 12:28:37 -0300

    testeando strider

--- test/errors/errorsTest.js
+++ test/errors/errorsTest.js
@@ -2,7 +2,6 @@
 
 const assert = require("assert");
 const Errors = require("../../lib/errors");
-const Boom = require("boom");
 
 describe("Controllers Errors Test", () => {
     it("Deberia traer un boom error que diga cannot access",() => {
