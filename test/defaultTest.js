9ea35194509989f8d22b0abc628ad077a42105d1
Pablo Damian Gomez <pgomez@despegar.com>
2016-08-05 11:29:05 -0300

    New test

--- test/defaultTest.js
+++ test/defaultTest.js
@@ -6,4 +6,7 @@ describe("Testeando SalesDB", () => {
     it("Deberia 1 ser igual a 1", () => {
         assert.equal(1 , 1);
     });
+    it("Deberia 2 ser igual a 2", () => {
+        assert.equal(2 , 2);
+    });
 });
